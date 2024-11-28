7817b5123fabb0ca098211c6f85f23960f827180
Oliver <owenger@ethz.ch>
2024-11-28 22:16:59 +0100

    restructured the transaction reader

--- src/transaction.py
+++ src/transaction.py
@@ -5,6 +5,7 @@ from datetime import date
 from enum import Enum
 from typing import List
 
+
 class Currency(str, Enum):
     CHF = 'CHF'
     USD = 'USD'
@@ -13,22 +14,20 @@ class Currency(str, Enum):
 
 
 @dataclass
-class SwiftTransaction:
+class Transaction:
     amount: float
     currency: Currency
     transaction_date: date
     detail: str
 
-class SwiftHandler:
-    def __init__(self):
-        self._transactions: List[SwiftTransaction] = []
-    
-    def load_from_file(self, path: str) -> bool:
+    @staticmethod
+    def list_from_swift_file(path: str) -> List["Transaction"]:
         if not os.path.isfile(path):
             print("Path does not exist, can't load swift file.")
             return False
         try:
             with open(path, 'r', encoding='latin1') as file:
+                transactions: List[Transaction] = []
                 data = mt940.parse(file.read())
                 for transaction in data.transactions:
                     amount: int = int(transaction.data['amount'].amount)
@@ -38,21 +37,19 @@ class SwiftHandler:
                     start = detail.find('?')
                     end = detail.find('\n', start)
                     detail: str = detail[start+1:end]
-                    self._transactions.append(SwiftTransaction(amount=amount, currency=currency, 
+                    transactions.append(Transaction(amount=amount, currency=currency, 
                                             transaction_date=transaction_date, detail=detail))
+                return transactions
         except Exception as e:
             print(f'File not readable. Failed with exception: {e}.')
             return False
-        
-
 
 
 def main():
     abs_path = os.path.abspath(__file__)
     path = f'{os.path.dirname(abs_path)}/../resources/transactions.mt940'
-
-    swift_handler = SwiftHandler()
-    swift_handler.load_from_file(path)
+    check = Transaction.list_from_swift_file(path)
+    print(check)
 
 if __name__ == '__main__':
     main()
